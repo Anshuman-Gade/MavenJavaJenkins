79a4f1b21286579447c2b17e91b3889bda15a06f
Anshuman-Gade <anshumangadey@gmail.com>
2022-12-11 19:09:30 +0530

    Demo for Team

---
+++ src/main/java/SEGroup/MavenJavaJenkins/App.java
@@ -0,0 +1,13 @@
+package SEGroup.MavenJavaJenkins;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
